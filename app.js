ebcdd968b47c98be3ebd720e26515e9862bee1d8
Madeline Stevens <stevensm1987@gmail.com>
2017-06-20 07:40:35 -0700

    committing after deleting usb.jpg

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 'use strict';
-
+//array of image objects
 var allProducts = [];
+//array for randomly generated images?
 var imageArray = [];
 
+//right, left and center slots for images
+var rightIndex;
+var leftIndex;
+var centerIndex;
+
+//constructor function
 function testProduct(name, filepath) {
   this.name = name;
   this.filepath = filepath;
@@ -11,8 +18,9 @@ function testProduct(name, filepath) {
   allProducts.push(this);
   imageArray.push(this);
 
-}
+};
 
+//instances
 var bag = new testProduct('bag', 'images/bag.jpg');
 var banana = new testProduct('banana', 'images/banana.jpg');
 var bathroom = new testProduct('bathroom', 'images/bathroom.jpg');
@@ -27,35 +35,31 @@ var pen = new testProduct('pen', 'images/pen.jpg');
 var petsweep = new testProduct('pet-sweep', 'images/pet-sweep.jpg');
 var scissors = new testProduct('scissors', 'images/scissors.jpg');
 var shark = new testProduct('shark', 'images/shark.jpg');
-var sweep = new testProduct('sweep', 'images/sweep.jpg');
-var tauntaun = new testProduct('tauntaun', 'images/tautaun.jpg');
+var sweep = new testProduct('sweep', 'images/sweep.png');
+var tauntaun = new testProduct('tauntaun', 'images/tauntaun.jpg');
 var unicorn = new testProduct('unicorn', 'images/unicorn.jpg');
 var usb = new testProduct('usb', 'images/usb.jpg');
 var watercan = new testProduct('watercan', 'images/water-can.jpg');
-var wineglass = new testProduct('wineglass', 'images/wineglass.jpg');
+var wineglass = new testProduct('wineglass', 'images/wine-glass.jpg');
 
-function randomPic1() {
-  var randomNum = Math.floor(Math.random() * allProducts.length);
-  var randomProduct = document.getElementById('img1');
-  randomProduct.src = imageArray[randomNum];
-}
+function randomProducts() {
+  function randomPics() {
+    for(var i = 0; i < 3; i++) {
+      rightIndex = Math.floor(Math.random() * allProducts.length);
+      leftIndex = Math.floor(Math.random() * allProducts.length);
+      centerIndex = Math.floor(Math.random() * allProducts.length);
+    }
+    var right = document.getElementById('right');
+    var left = document.getElementById('left');
+    var center = document.getElementById('center');
 
-function randomPic2() {
-  var randomNum = Math.floor(Math.random() * allProducts.length);
-  var randomProduct = document.getElementById('img2');
-  randomProduct.src = imageArray[randomNum];
-}
+    right.src = allProducts[rightIndex].filepath;
+    left.src = allProducts[leftIndex].filepath;
+    center.src = allProducts[centerIndex].filepath;
+  }
+  randomPics();
+};
+randomProducts();
 
-function randomPic3() {
-  var randomNum = Math.floor(Math.random() * allProducts.length);
-  var randomProduct = document.getElementById('img3');
-  randomProduct.src = imageArray[randomNum];
-}
-
-function allRandom () {
-  randomPic1();
-  randomPic2();
-  randomPic3();
-}
-
-allRandom();
+//sam's code....
+// Product.left.src = Product.all[at[0, or 1..] ]
